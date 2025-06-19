c13397769d4b135a350746dad0e85ba6f19aad59
P-Nastia <nastia.pyvovarchuck@gmail.com>
2025-06-19 20:35:26 +0300

    create category mutation

--- src/services/apiCategory.ts
+++ src/services/apiCategory.ts
@@ -1,6 +1,7 @@
 import {createApi} from "@reduxjs/toolkit/query/react";
-import type {ICategoryItem} from "./types.ts";
+import type {ICategoryCreate, ICategoryItem} from "./types.ts";
 import {createBaseQuery} from "../utilities/createBaseQuery.ts";
+import {serialize} from "object-to-formdata";
 
 export const apiCategory = createApi({
     reducerPath: 'api',
@@ -9,8 +10,23 @@ export const apiCategory = createApi({
         getAllCategories: builder.query<ICategoryItem, void>({
             query: () => ''
         }),
+        createCategory: builder.mutation<ICategoryItem, ICategoryCreate>({ // спочатку те, що вертає, потім -- шо вертає запит
+            query:(newCategory) =>{
+                try {
+                    const formData= serialize(newCategory); // перетворення JSON
+                    return {
+                        url: '',
+                        method: 'POST',
+                        body: formData
+                    }
+                }
+                catch {
+                    throw new Error('Error create category');
+                }
+}
+        })
     }),
 });
 
 // queryrtk 
-export const { useGetAllCategoriesQuery } = apiCategory;
+export const { useGetAllCategoriesQuery,useCreateCategoryMutation } = apiCategory;
