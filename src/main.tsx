b5975c34769b9ea1f4fb2c8444e919717d699456
P-Nastia <nastia.pyvovarchuck@gmail.com>
2025-06-19 19:18:04 +0300

    is good working admin panel

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import "swiper/swiper-bundle.css";
+import "flatpickr/dist/flatpickr.css";
 import App from './App.tsx'
 import {Provider} from "react-redux";
 import {store} from "./store";
+import {ThemeProvider} from "./context/ThemeContext.tsx";
+import {AppWrapper} from "./components/common/PageMeta.tsx";
 
 createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+    <>
+        <ThemeProvider>
+            <AppWrapper>
+                <Provider store={store}>
+                    <App/>
+                </Provider>,
+            </AppWrapper>
+        </ThemeProvider>,
+    </>
 )
